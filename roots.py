2b588d238630ddfcc61c5406467fc6cda48e0988
Aman Kumar <27aman113114@gmil.com>
2026-02-23 20:14:35 +0530

    imaginary

--- roots.py
+++ roots.py
@@ -7,8 +7,11 @@ c=float(input("enter constant term : "))
 
 D=((b**2) - (4*a*c))
 if D>=0:
-    root1=(-b+D**(1/2))/(2*a)
+    root1=(-b+math.sqrt(D))/(2*a)
     root2=(-b-D**(1/2))/(2*a)
     print(f"the roots are {root1} and {root2}")
 else:
-    print("roots does not exist")
+    print("real roots does not exist")
+    real_part=-b/(2*a)
+    imag_part=math.sqrt(abs(D))/(2*a)
+    print(f"the  imaginary roots are {real_part} + {(imag_part)}i and {real_part} - {(imag_part)}i")
